4b1d4c793b38f3aa53cdb75a3d26d64323251766
ProZoom <18642118+ProZoom@users.noreply.github.com>
2017-08-10 19:22:46 +0800

    Initial commit sava data

---
+++ app/src/main/java/com/top/wisecity/newscenterpage/PhotosBaseNewsCenterPage.java
@@ -0,0 +1,35 @@
+package com.top.wisecity.newscenterpage;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.top.wisecity.activity.MainActivity;
+
+
+/**
+ * @author 李阳
+ * @创建时间 2017/3/22 - 下午10:15
+ * @描述
+ * @ 当前版本:
+ */
+
+public class PhotosBaseNewsCenterPage extends BaseNewsCenterPage {
+
+    public PhotosBaseNewsCenterPage(MainActivity mainActivity) {
+        super(mainActivity);
+    }
+
+    @Override
+    public View initView() {
+        //要展示的内容
+
+        TextView tv=new TextView(mainActivity);
+        tv.setText("组图的内容");
+        tv.setTextSize(25);
+        tv.setGravity(Gravity.CENTER);
+        tv.setTextColor(Color.BLUE);
+        return tv;
+    }
+}
